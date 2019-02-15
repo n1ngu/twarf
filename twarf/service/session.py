c6d67302c4bdebb54a3b83292e0e84e83c738aff
Ningú <47453810+n1ngu@users.noreply.github.com>
2019-02-15 20:23:24 +0100

    Refactor session service

--- twarf/service/session.py
+++ twarf/service/session.py
@@ -21,30 +21,15 @@ class SessionService():
     def __init__(self):
         self._sessions = {}
 
-    async def _get(self, key):
+    async def get(self, key):
         return self._sessions.get(key)
 
-    def get(self, key):
-        return twisted.internet.defer.ensureDeferred(
-            self._get(key)
-        )
-
-    async def _put(self, key, value):
+    async def put(self, key, value):
         self._sessions[key] = value
 
-    def put(self, key, value):
-        return twisted.internet.defer.ensureDeferred(
-            self._put(key, value)
-        )
-
-    async def _new(self, value=0):
+    async def new(self, value=0):
         key = ''.join(
             random.choice(self._BASE) for x in range(self._LEN)
         ).encode('ascii')
-        await self._put(key, value)
+        await self.put(key, value)
         return key
-
-    def new(self, value=0):
-        return twisted.internet.defer.ensureDeferred(
-            self._new(value)
-        )

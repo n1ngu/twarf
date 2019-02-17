7a0abcf915a227d8b2481bd8474baba6b3da064e
Ningú <47453810+n1ngu@users.noreply.github.com>
2019-02-17 23:16:54 +0100

    Implement html meta redirect challenge

--- twarf/rules/challenge.py
+++ twarf/rules/challenge.py
@@ -15,6 +15,14 @@ from .session import MatchCookie
 
 
 class RedirectChallenge(Finish):
+    """
+    Redirect through a 307 response
+
+    Drawbacks:
+      - If it happens too often users might get stuck on pages, unable
+        to navigate backwards
+      - Easily spoofable
+    """
 
     def __init__(self,
                  next_challenge,
@@ -30,7 +38,7 @@ class RedirectChallenge(Finish):
             request.uri
         )
         passed = await self.crypto_srv.decrypt(uri.path[1:])
-        if passed and passed == b'challenge1':
+        if passed and passed == b'challengeA':
             cookie = request.received_cookies.get(COOKIE)
             query = urllib.parse.parse_qs(uri.query)
             quoted_referers = query.get(b'referer')
@@ -41,13 +49,62 @@ class RedirectChallenge(Finish):
             request.temporary_redirect(referer)
             await super().process(request)
         else:
-            challenge = await self.crypto_srv.encrypt(b'challenge1')
+            challenge = await self.crypto_srv.encrypt(b'challengeA')
             referer = urllib.parse.quote_from_bytes(request.uri).encode()
             query = urllib.parse.urlencode({b'referer': referer}).encode()
             location = urllib.parse.urlunparse(
                 (None, None, challenge, None, query, None)
             )
             request.temporary_redirect(location)
+            await super().process(request)
+
+
+class MetaRedirectChallenge(Finish):
+    """
+    Redirect through a page with a <meta http-equiv="refresh"/> html tag
+
+    Drawbacks:
+      - If it happens too often users might get stuck on pages, unable
+        to navigate backwards
+      - Original requests won't be replayed (POST, PUT, ...)
+    """
+    TPL = b"""
+    <meta http-equiv="refresh" content="0; url=%s" />
+    """
+
+    def __init__(self,
+                 next_challenge,
+                 crypto_srv,
+                 session_srv,
+                 ):
+        self.next_challenge = next_challenge
+        self.crypto_srv = crypto_srv
+        self.session_srv = session_srv
+
+    async def process(self, request):
+        uri = urllib.parse.urlparse(
+            request.uri
+        )
+        passed = await self.crypto_srv.decrypt(uri.path[1:])
+        if passed and passed == b'challengeB':
+            cookie = request.received_cookies.get(COOKIE)
+            query = urllib.parse.parse_qs(uri.query)
+            quoted_referers = query.get(b'referer')
+            if not quoted_referers:
+                raise NotImplementedError("")  # TODO: 400 BadRequest
+            referer = urllib.parse.unquote_to_bytes(quoted_referers[0])
+            await self.session_srv.put(cookie, self.next_challenge)
+            request.temporary_redirect(referer)
+            await super().process(request)
+        else:
+            challenge = await self.crypto_srv.encrypt(b'challengeB')
+            referer = urllib.parse.quote_from_bytes(request.uri).encode()
+            query = urllib.parse.urlencode({b'referer': referer}).encode()
+            location = urllib.parse.urlunparse(
+                (None, None, challenge, None, query, None)
+            )
+            page = self.TPL % location
+            request.write(page)
             await super().process(request)
 
 
@@ -71,7 +128,7 @@ def twarf_rules(reactor) -> TwarfRule:
         then=Forward(reactor),
         orelse=If(
             test=MatchCookie(session_srv, b'challenge1'),
-            then=RedirectChallenge(b'valid', crypto_srv, session_srv),
+            then=MetaRedirectChallenge(b'valid', crypto_srv, session_srv),
             orelse=SetCookie(session_srv, b'challenge1'),
         ),
     )

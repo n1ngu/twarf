a4db6678e461f6d5b92ab1365a9de4945263bb82
Ningú <47453810+n1ngu@users.noreply.github.com>
2019-02-21 12:31:55 +0100

    Test http status response codes

--- tests/rules/test_http.py
+++ tests/rules/test_http.py
@@ -2,7 +2,10 @@
 import aiounittest
 import twisted.web.test.requesthelper
 
-from twarf.rules.http import Finish, BadRequest
+from twarf.rules.http import Finish
+from twarf.rules.http import Unauthorized
+from twarf.rules.http import BadRequest
+from twarf.rules.http import InternalServerError
 
 
 class FinishTest(aiounittest.AsyncTestCase):
@@ -17,6 +20,23 @@ class FinishTest(aiounittest.AsyncTestCase):
         self.assertTrue(request.responseHeaders.hasHeader(b'date'))
 
 
+# TODO: TempRedirectTest (request.temporary_redirect is a
+# TwarfRequest-only feature)
+
+
+class UnauthorizedTest(aiounittest.AsyncTestCase):
+
+    async def test_rule(self):
+        request = twisted.web.test.requesthelper.DummyRequest(b'')
+
+        await Unauthorized()(request)
+
+        self.assertEqual(request.responseCode, 401)
+        self.assertTrue(request.finished)
+        self.assertTrue(request.responseHeaders.hasHeader(b'server'))
+        self.assertTrue(request.responseHeaders.hasHeader(b'date'))
+
+
 class BadRequestTest(aiounittest.AsyncTestCase):
 
     async def test_rule(self):
@@ -25,6 +45,19 @@ class BadRequestTest(aiounittest.AsyncTestCase):
         await BadRequest()(request)
 
         self.assertEqual(request.responseCode, 400)
+        self.assertTrue(request.finished)
+        self.assertTrue(request.responseHeaders.hasHeader(b'server'))
+        self.assertTrue(request.responseHeaders.hasHeader(b'date'))
+
+
+class InternalServerErrorTest(aiounittest.AsyncTestCase):
+
+    async def test_rule(self):
+        request = twisted.web.test.requesthelper.DummyRequest(b'')
+
+        await InternalServerError()(request)
+
+        self.assertEqual(request.responseCode, 500)
         self.assertTrue(request.finished)
         self.assertTrue(request.responseHeaders.hasHeader(b'server'))
         self.assertTrue(request.responseHeaders.hasHeader(b'date'))
